1b49565fa77c69d2bb943415a2639d39636b6a51
Crementif <26669564+Crementif@users.noreply.github.com>
2023-02-24 21:00:42 +0100

    Remove all assets from previous game

--- source/game/GameSceneIngame.h
+++ source/game/GameSceneIngame.h
@@ -24,18 +24,11 @@ public:
 
 private:
     void DrawBackground();
-    AABB GetHotbarPosition();
-    AABB GetHotbarButtonPosition(u32 buttonIndex);
-    void DrawMenu();
+    void DrawHUD();
 
     void UpdateCamera();
 
     AABB GetWorldBounds();
-    AABB GetDeleteAreaBounds();
-
-    void StartTouch(Vector2f screenPos);
-    void UpdateTouch(Vector2f screenPos);
-    void EndTouch();
 
     Sprite m_testSprite{"/tex/cross.tga", true};
 
@@ -51,38 +44,9 @@ private:
     std::unique_ptr<RelayServer> m_server;
     std::unique_ptr<RelayClient> m_client;
 
-    // hotbar / menu graphics
-    Sprite m_menuBackdrop{"/tex/button_backdrop.tga", true};
-    Sprite m_menuItemBeamHorizontalShort{"/tex/menu_beam.tga", true};
-    Sprite m_menuItemBeamVerticalLong{"/tex/menu_beam_2.tga", true};
-    Sprite m_menuItemSplitter{"/tex/menu_splitter.tga", true};
-    Sprite m_menuSpeedBoost{"/tex/speed_boost_0.tga", true};
-    Sprite m_menuItemSelected{"/tex/menu_item_selected.tga", true};
-    Sprite m_menuItemDeselected{"/tex/menu_item_deselected.tga", true};
-
-    Sprite m_spriteDeleteArea{"/tex/trash_symbol.tga"};
-    Sprite m_spriteDeleteAreaHL{"/tex/trash_symbol_hl.tga"};
-
-    Sprite m_spriteGlow128{"/tex/glow128.tga", true};
-
-    // touch
-    bool m_isTouching{false};
-    Vector2f m_startTouchPosition;
-
     // touch scrolling
     bool m_isScrolling{false};
     Vector2f m_startCameraPosition;
-
-    // touch building
-    bool m_isBuildMode{false};
-    BUILDTYPE m_buildType{};
-
-    // touch dragging / delete
-    bool m_isDragMode{false};
-    class Object* m_dragObject{nullptr};
-    Vector2f m_initialObjectPosition;
-    Vector2f m_grabOffset; // grab offset, relative to top-left corner of currently dragged object
-    bool m_isHoveringDeleteArea{false};
 
     // misc
     f32 m_gameTime{};
