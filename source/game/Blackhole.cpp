63cbd6c94882c87a4a02a911b069d29bd06ce876
Crementif <26669564+Crementif@users.noreply.github.com>
2023-12-31 18:41:17 +0100

    Add audio to blackhole
    It's a simple but effective sinewave

--- source/game/Blackhole.cpp
+++ source/game/Blackhole.cpp
@@ -7,19 +7,31 @@ Blackhole::Blackhole(GameScene *parent, u32 owner, float x, float y, float volX,
     // the blackhole will have a constant velocity
     this->SetVelocity(volX, volY);
     m_velocityAngle = Vector2f(0.0f, 0.0f).AngleTowards(m_velocity)+(float)M_PI_2;
+
+    m_loopSfx = new Audio("/sfx/gravity_loop.wav");
+    m_loopSfx->SetLooping(true);
+    m_endSfx = new Audio("/sfx/gravity_end.wav");
 }
 
 void Blackhole::Update(float timestep) {
-    m_shootAnimationIdx+=2;
-    if (m_shootAnimationIdx >= 30)
+    m_shootAnimationIdx++;
+    if (m_shootAnimationIdx >= 15)
         m_shootAnimationIdx = 0;
 
     if (!m_imploding) {
+        float distance = (m_parent->GetPlayer()->GetPosition().Distance(this->m_aabb.GetCenter())+0.00000001f)/20.0f;
+        float volume = 20.0f - (distance/100.0f*20.0f);
+
+        if (m_loopSfx->GetState() == Audio::StateEnum::LOADED)
+            m_loopSfx->Play();
+        m_loopSfx->SetVolume((u32)volume);
+        m_loopSfx->SetLooping(true);
+
         this->SimulatePhysics();
     }
     else {
         constexpr u32 m_implosionStep = 3;
-        constexpr u32 m_implosionStepLength = 10;
+        constexpr u32 m_implosionStepLength = 5;
 
         if (m_implosionAnimationIdx == 0) {
             if (m_parent->GetPlayer()->GetPlayerId() == m_owner) {
@@ -41,10 +53,11 @@ void Blackhole::Update(float timestep) {
         float distance = (m_parent->GetPlayer()->GetPosition().Distance(this->m_aabb.GetCenter())+0.00000001f)/20.0f;
         float volume = 20.0f - (distance/100.0f*20.0f);
 
-        Audio* explosionAudio = new Audio("/sfx/explosion.wav");
-        explosionAudio->Play();
-        explosionAudio->SetVolume((u32)volume);
-        explosionAudio->QueueDestroy();
+        m_loopSfx->SetLooping(false);
+        m_loopSfx->QueueDestroy();
+        m_endSfx->Play();
+        m_endSfx->SetVolume((u32)volume);
+        m_endSfx->QueueDestroy();
     }
 }
 
@@ -54,11 +67,11 @@ Vector2f Blackhole::GetPosition() {
 
 void Blackhole::Draw(u32 layerIndex) {
     u32 size = !m_imploding ? 32 : (m_implosionAnimationIdx <= 20 ? 32 + (m_implosionAnimationIdx/2) : 32 + (20/2) - (m_implosionAnimationIdx));
-    if (m_shootAnimationIdx >= 20) {
+    if (m_shootAnimationIdx >= 10) {
         Render::RenderSprite(s_blackhole2Sprite, (s32)m_aabb.pos.x*MAP_PIXEL_ZOOM, (s32)m_aabb.pos.y*MAP_PIXEL_ZOOM, size * 1.5, size * 1.5, m_velocityAngle);
     }
-    else if (m_shootAnimationIdx >= 10) {
-        Render::RenderSprite(s_blackhole1Sprite, (s32)m_aabb.pos.x*MAP_PIXEL_ZOOM, (s32)m_aabb.pos.y*MAP_PIXEL_ZOOM, size * 1.55, size * 1.55, m_velocityAngle);
+    else if (m_shootAnimationIdx >= 5) {
+        Render::RenderSprite(s_blackhole1Sprite, (s32)m_aabb.pos.x*MAP_PIXEL_ZOOM, (s32)m_aabb.pos.y*MAP_PIXEL_ZOOM, size * 1.6, size * 1.6, m_velocityAngle);
     }
     else {
         Render::RenderSprite(s_blackhole0Sprite, (s32)m_aabb.pos.x*MAP_PIXEL_ZOOM, (s32)m_aabb.pos.y*MAP_PIXEL_ZOOM, size * 1.5, size * 1.5, m_velocityAngle);
--- source/game/Blackhole.h
+++ source/game/Blackhole.h
@@ -8,7 +8,6 @@ class Blackhole : public PhysicsObject {
 
 public:
     Blackhole(GameScene* parent, u32 owner, float x, float y, float volX, float volY);
-
     u32 GetOwner() const { return m_owner; }
 
     static Sprite* s_blackhole0Sprite;
@@ -21,6 +20,8 @@ private:
     u32 m_shootAnimationIdx = 0;
     u32 m_implosionAnimationIdx = 0;
     float m_velocityAngle = 0.0f;
+    class Audio* m_loopSfx = nullptr;
+    class Audio* m_endSfx = nullptr;
     void Draw(u32 layerIndex) override;
     void Update(float timestep) override;
     Vector2f GetPosition() override;
